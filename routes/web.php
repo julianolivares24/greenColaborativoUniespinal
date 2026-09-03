a17cce516b4c435c70e23456a9c66d413e42e3b7
Julian Olivares <julianolivaresg24@hotmail.com>
2026-09-03 14:55:27 -0500

    repairing fix

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SaludoController;
 
+Route::get('/', [SaludoController::class, 'index']);
 Route::get('/saludo', [SaludoController::class, 'index']);
+
 
